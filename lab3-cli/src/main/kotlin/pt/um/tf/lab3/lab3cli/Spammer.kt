3904a258c7ba2fadda2c664b396ddf5e9532e0ed
André Diogo <redan.blue27@gmail.com>
2018-06-27 01:23:03 +0100

    Adds logging.

--- lab3-cli/src/main/kotlin/pt/um/tf/lab3/lab3cli/Spammer.kt
+++ lab3-cli/src/main/kotlin/pt/um/tf/lab3/lab3cli/Spammer.kt
@@ -1,23 +1,26 @@
 package pt.um.tf.lab3.lab3cli
 
+import mu.KLogging
 import java.util.concurrent.BlockingQueue
 
-class Spammer(val i: Int, val bf: BankFactory, val q: BlockingQueue<Long>) {
+class Spammer(private val i: Int, private val bf: BankFactory, private val q: BlockingQueue<Long>) {
+    companion object : KLogging()
     fun execute() {
+        logger.info("New spammer $i")
         val b = bf.newBank()
         val rand = (20000..80000).random()
         var balance : Long = 0
-        println("$i Spammer will do $rand iterations")
+        logger.info("$i Spammer will do $rand iterations")
         (0..rand).forEach {
             val mov = (-200..200).random().toLong()
-            if (b.movement(mov)) {
+            if (!b.movement(mov)) {
                 balance += mov
             }
             else {
-                println("Rejected $mov")
+                logger.info("Rejected $mov")
             }
         }
-        println("$i Spammer finished with $balance")
+        logger.info("$i Spammer finished with $balance")
         q.put(balance)
     }
 }
