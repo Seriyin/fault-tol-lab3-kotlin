218ca8843a4863d6c9540f9c083ee4997cb3eea1
André Diogo <redan.blue27@gmail.com>
2018-06-27 01:23:42 +0100

    Fixes lack of default parameters for no-arg constructors.

--- lab3-mes/src/main/kotlin/pt/um/tf/lab3/lab3mes/Message.kt
+++ lab3-mes/src/main/kotlin/pt/um/tf/lab3/lab3mes/Message.kt
@@ -4,14 +4,13 @@ import io.atomix.catalyst.buffer.BufferInput
 import io.atomix.catalyst.buffer.BufferOutput
 import io.atomix.catalyst.serializer.CatalystSerializable
 import io.atomix.catalyst.serializer.Serializer
-import java.util.*
 
 /**
  * Message with operation.
  */
 data class Message(var op : Int = 0,
                    var mov : Long = 0,
-                   var origin : String) : CatalystSerializable {
+                   var origin : String = "") : CatalystSerializable {
     override fun writeObject(buffer: BufferOutput<*>?, serializer: Serializer?) {
         buffer?.writeInt(op)
               ?.writeLong(mov)
--- lab3-mes/src/main/kotlin/pt/um/tf/lab3/lab3mes/UpdateMessage.kt
+++ lab3-mes/src/main/kotlin/pt/um/tf/lab3/lab3mes/UpdateMessage.kt
@@ -5,7 +5,7 @@ import io.atomix.catalyst.buffer.BufferOutput
 import io.atomix.catalyst.serializer.CatalystSerializable
 import io.atomix.catalyst.serializer.Serializer
 
-data class UpdateMessage(var accbalance : Long) : CatalystSerializable {
+class UpdateMessage(var accbalance : Long = 0) : CatalystSerializable {
     override fun writeObject(buffer: BufferOutput<*>?, serializer: Serializer?) {
         buffer?.writeLong(accbalance)
     }
